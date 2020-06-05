0ee6f20445978225a2576bdd54d514c204df7ba9
Walter Delevich <walter.delevich@gmail.com>
2020-06-04 21:15:35 -0400

    Update splash

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>Services That Are Hard to Unsubscribe From.</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>NYTimes makes it hard to unsubscribe.</h3>
 
+    <footer>And that ain't cool.</footer>
     <style jsx>{`
       :global(html,body) {
         margin: 0;
